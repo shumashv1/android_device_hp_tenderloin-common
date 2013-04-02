75851de81b242544fc9ba335c82565f3fc9a2001
David Marble <drmarble1@gmail.com>
2013-04-01 20:54:25 -0500

    add TARGET_ARCH := arm for cm10.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 BOARD_USES_ADRENO_200 := true
 
 TARGET_CPU_ABI := armeabi-v7a
+TARGET_ARCH := arm
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
