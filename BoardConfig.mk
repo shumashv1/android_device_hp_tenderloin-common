80a45913710e14ed718ccc95a455c885ebedd96e
Micha LaQua <micha.laqua@gmail.com>
2014-12-06 14:41:52 +0100

    recovery: use w720 assets

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,7 @@ TARGET_KERNEL_MODULES := KERNEL_WIFI_MODULES
 TARGET_RECOVERY_DEVICE_DIRS += device/hp/tenderloin
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/hp/tenderloin/recovery/recovery_ui.c
 BOARD_CUSTOM_GRAPHICS:= ../../../device/hp/tenderloin/recovery/graphics.c
+TARGET_RECOVERY_SCREEN_WIDTH := 720
 RECOVERY_FSTAB_VERSION=2
 TARGET_RECOVERY_FSTAB = device/hp/tenderloin/recovery/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
