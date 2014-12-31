617710ca3426f01162acf51fd68527017abc1ab1
shumash <shumashgeely@gmail.com>
2014-12-31 09:40:22 -0700

    add 3.4 kernel ion.h header file

--- include/linux/msm_ion.h
+++ include/linux/msm_ion.h
@@ -16,7 +16,7 @@
 #ifndef _LINUX_MSM_ION_H
 #define _LINUX_MSM_ION_H
 
-#include <linux/ion.h>
+//#include <linux/ion.h>
 
 enum msm_ion_heap_types {
 	ION_HEAP_TYPE_MSM_START = ION_HEAP_TYPE_CUSTOM + 1,
