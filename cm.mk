56767ff826302c48409644662a376b6c3c21a8bc
Micha LaQua <micha.laqua@gmail.com>
2015-01-13 15:14:36 +0100

    update fingerprint for LP

--- cm.mk
+++ cm.mk
@@ -14,4 +14,4 @@ PRODUCT_BRAND := HP
 PRODUCT_MODEL := TouchPad
 PRODUCT_MANUFACTURER := HP
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.4.4/KTU84P/1227136:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.4.4 KTU84P 1227136 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:5.0.2/LRX22G/1649326:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 5.0.2 LRX22G 1649326 release-keys"
